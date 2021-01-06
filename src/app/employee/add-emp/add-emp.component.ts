9e900f372589382b4e3b10ea0ceab482621c929a
Shipra Saha <shiprasaha.gbpec@gmail.com>
2021-01-07 00:04:04 +0100

    commit 4

--- src/app/employee/add-emp/add-emp.component.ts
+++ src/app/employee/add-emp/add-emp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-emp',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-emp.component.css']
 })
 export class AddEmpComponent implements OnInit {
+  _id: number;
+  static latestId: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: SharedService) {
+    this._id = AddEmpComponent.incrementId()
   }
 
+  @Input() emp: any;
+  EmployeeId: string;
+  EmployeeName: string;
+  Department: string;
+  DateOfJoining: string;
+
+  DepartmentsList: any = [];
+
+  ngOnInit(): void {
+    this.loadDepartmentList();
+  }
+
+  loadDepartmentList() {
+    this.service.getAllDepartmentNames().subscribe((data: any) => {
+      this.DepartmentsList = data;
+
+      // this.EmployeeId = this.emp.EmployeeId;
+      // this.EmployeeName = this.emp.EmployeeName;
+      // this.Department = this.emp.Department;
+      // this.DateOfJoining = this.emp.DateOfJoining;
+    });
+  }
+
+  addEmployee() {
+    var val = {
+      EmployeeId: this.EmployeeId,
+      EmployeeName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining,
+    };
+
+    this.service.addEmployee(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  static incrementId() {
+    !this.latestId? 1 :this.latestId++;
+    return this.latestId;
+  }
 }
--- src/app/employee/show-emp/show-emp.component.ts
+++ src/app/employee/show-emp/show-emp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-emp',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowEmpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  EmployeeList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
 
   ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  addClick(){
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:"",
+      Department:"",
+      DateOfJoining:"",
+    }
+    this.ModalTitle="Add Employee";
+    this.ActivateAddEditEmpComp=true;
+
+  }
+
+  editClick(item){
+    console.log(item);
+    this.emp=item;
+    this.ModalTitle="Edit Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  closeClick(){
+    this.ActivateAddEditEmpComp=false;
+    this.refreshEmpList();
+  }
+
+
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+    });
   }
 
 }
